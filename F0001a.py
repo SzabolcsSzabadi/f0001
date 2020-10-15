c7761ab8f7ee1a3bbb46d7795151440d0763398a
SzabolcsSzabadi <55064779+SzabolcsSzabadi@users.noreply.github.com>
2020-10-15 12:14:51 +0200

    Add files via upload

---
+++ F0001a.py
@@ -0,0 +1,11 @@
+'''
+print('...Hogyha én ezt tudtam volna)
+print('Köpönyeget hoztam vóna'!)
+'''
+
+#Az elsőnél syntax hibát fogunk kapni mert a végén nincs aposztróf
+#A másodiknál is syntax hibát fogunk kapni mert a felkiáltó jel az aposztrófokon kivűl van
+
+#Javitás:
+print('...Hogyha én ezt tudtam volna')
+print('Köpönyeget hoztam vóna!')
---
+++ f0001b.py
@@ -0,0 +1,8 @@
+'''Mi történik, ha lefuttatjuk ezt a programot
+
+print('Rásír a sok kis malac is!')
+print('"Én is, én is, én is, én is!"')
+'''
+
+#Az elsőnél ezt fogja kiírni: Rásír a sok kis malac is!
+#A másodiknál ezt fogja kiírni macskakörmökkel együtt: "Én is, én is, én is, én is"
